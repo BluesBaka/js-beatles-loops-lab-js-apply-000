c99297e54efca6b48df4c310d639ad39470f4244
Bryna Dunnells <badunnells@gmail.com>
2019-04-23 17:14:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // add solution here
+
+
+const arr = []
+
+function theBeatlesPlay(musicians,instruments){
+          for (let i=0, l=musicians.length; i<l; i++) {
+        arr.push(musicians[i]+ " plays " +instruments[i])
+      }
+
+      return arr
+};
+
+
+
+ function johnLennonFacts(facts){
+   const factastic = []
+   let i = 0
+        while(i<facts.length){
+          factastic.push(facts[i]+"!!!")
+          i++
+      };
+
+      return factastic
+    };
+
+function iLoveTheBeatles(i){
+    const loveMeDo =[]
+
+      do {
+        loveMeDo.push ("I love the Beatles!");
+
+      i++;
+      }
+      while (i>15)
+      
+};
